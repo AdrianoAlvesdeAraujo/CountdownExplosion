fdb99d3d693ab0bebe5b155eda18ed74cf22bdb4
Adriano Araújo <adrianoalvescru@gmail.com>
2019-07-28 10:29:40 -0300

    projeto em javascript - contagem regressiva para explosão

---
+++ CountdownExplosion/countdown.js
@@ -0,0 +1,47 @@
+/**
+ * JS - Aula10: Countdown
+ * @author Adriano Alves
+ */
+
+var check = false;
+
+function xequeMate() {
+  if (check == false) {
+    var timer1 = setInterval(function() {
+      start();
+    }, 1000);
+    var timer2 = setTimeout(function() {
+      end();
+    }, 13200);
+    var count = 10;
+    function start() {
+      soundBeep();
+      document.getElementById("time").innerHTML = count;
+      if (count == 0) {
+        soundThunder();
+        clearInterval(timer1);
+        document.getElementById("fire").src = "explosion.gif";
+        document.getElementById("time").innerHTML = "GAME OVER";
+      }
+      count--;
+    }
+    check = true;
+  } else {
+  }
+}
+
+function soundThunder() {
+  var beep = new Audio();
+  beep.src = "Thunder_Crack.mp3";
+  beep.play();
+}
+
+function soundBeep() {
+  var beep = new Audio();
+  beep.src = "Beep_Short.mp3";
+  beep.play();
+}
+
+function end() {
+  document.getElementById("fire").src = "clean.png";
+}
